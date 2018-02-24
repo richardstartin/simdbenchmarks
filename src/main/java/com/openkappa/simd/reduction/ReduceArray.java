37a737d23fafaa7a49429b9898521c2362f5c573
Richard Startin <richard@openkappa.co.uk>
2018-02-24 22:05:41 +0000

    unroll vs vectorise blog post

--- src/main/java/com/openkappa/simd/reduction/ReduceArray.java
+++ src/main/java/com/openkappa/simd/reduction/ReduceArray.java
@@ -2,6 +2,7 @@ package com.openkappa.simd.reduction;
 
 import org.openjdk.jmh.annotations.*;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import static com.openkappa.simd.DataUtil.createDoubleArray;
@@ -10,16 +11,31 @@ import static com.openkappa.simd.DataUtil.createDoubleArray;
 @State(Scope.Thread)
 public class ReduceArray {
 
+  public static void main(String[] args) {
+    ReduceArray benchmark = new ReduceArray();
+    benchmark.size = 65536;
+    benchmark.setup();
+    System.out.println(benchmark.reducePaginated());
+    System.out.println(benchmark.reduceUnrolledPaginated());
+  }
+
+
   @Param({"1024",
           "65536",
-          "131072"
+          "1048576"
   })
   int size;
   private double[] data;
 
+  private double[][] paginated;
+
   @Setup(Level.Trial)
   public void setup() {
     this.data = createDoubleArray(size);
+    this.paginated = new double[size/1024][];
+    for (int i = 0; i < size / 1024; ++i) {
+      paginated[i] = createDoubleArray(1024);
+    }
   }
 
   @Benchmark
@@ -36,6 +52,33 @@ public class ReduceArray {
     return reduce(buffer);
   }
 
+  @Benchmark
+  public double reduceUnrolled() {
+    double a0 = 0.0;
+    double a1 = 0.0;
+    double a2 = 0.0;
+    double a3 = 0.0;
+    for (int i = 0; i < data.length >>> 2; i++) {
+      a0 += data[i * 8 + 0];
+      a1 += data[i * 8 + 1];
+      a2 += data[i * 8 + 2];
+      a3 += data[i * 8 + 3];
+    }
+    return a0 + a1 + a2 + a3;
+  }
+
+
+  @Benchmark
+  public double reducePaginated() {
+    double[] buffer = Arrays.copyOf(paginated[0], paginated[0].length);
+    for (int i = 1; i < paginated.length; ++i) {
+      double[] page = paginated[i];
+      for (int j = 0; j < page.length && j < buffer.length; ++j) {
+        buffer[j] += page[j];
+      }
+    }
+    return reduceUnrolled(buffer);
+  }
 
   @Benchmark
   public double reduceVectorised() {
@@ -48,6 +91,38 @@ public class ReduceArray {
       }
     }
     return reduce(buffer);
+  }
+
+  @Benchmark
+  public double reduceUnrolledPaginated() {
+    double a0 = 0.0;
+    double a1 = 0.0;
+    double a2 = 0.0;
+    double a3 = 0.0;
+    for (int i = 0; i < paginated.length; ++i) {
+      double[] page = paginated[i];
+      for (int j = 0; j < paginated[0].length; j += 4) {
+        a0 += page[j + 0];
+        a1 += page[j + 1];
+        a2 += page[j + 2];
+        a3 += page[j + 3];
+      }
+    }
+    return a0 + a1 + a2 + a3;
+  }
+
+  private double reduceUnrolled(double[] data) {
+    double a0 = 0.0;
+    double a1 = 0.0;
+    double a2 = 0.0;
+    double a3 = 0.0;
+    for (int i = 0; i < data.length >>> 2; i++) {
+      a0 += data[i * 4 + 0];
+      a1 += data[i * 4 + 1];
+      a2 += data[i * 4 + 2];
+      a3 += data[i * 4 + 3];
+    }
+    return a0 + a1 + a2 + a3;
   }
 
 
---
+++ src/main/java/com/openkappa/simd/sumproduct/SumProduct.java
@@ -0,0 +1,96 @@
+package com.openkappa.simd.sumproduct;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.openkappa.simd.DataUtil.createDoubleArray;
+import static com.openkappa.simd.DataUtil.createFloatArray;
+
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class SumProduct {
+
+  @Param({//"1024",
+         "65536"
+  })
+  int size;
+  private double[] xd;
+  private double[] yd;
+  private float[] xs;
+  private float[] ys;
+
+  @Setup(Level.Trial)
+  public void init() {
+    this.xd = createDoubleArray(size);
+    this.yd = createDoubleArray(size);
+    this.xs = createFloatArray(size);
+    this.ys = createFloatArray(size);
+  }
+
+  public static void main(String[] args) {
+    SumProduct b = new SumProduct();
+    b.size = 1024;
+    b.init();
+    System.out.println(b.unrolledDoubleSumProduct());
+    System.out.println(b.vectorisedDoubleSumProduct());
+  }
+
+
+  @Benchmark
+  public double vectorisedDoubleSumProduct() {
+    double sp = 0D;
+    for (int i = 0; i < xd.length && i < yd.length; ++i) {
+      sp += xd[i] * yd[i];
+    }
+    return sp;
+  }
+
+  @Benchmark
+  public double unrolledDoubleSumProduct() {
+    double sp1 = 0D;
+    double sp2 = 0D;
+    double sp3 = 0D;
+    double sp4 = 0D;
+    for (int i = 0; i < xd.length && i < yd.length; i += 4) {
+      sp1 += xd[i] * yd[i];
+      sp2 += xd[i + 1] * yd[i + 1];
+      sp3 += xd[i + 2] * yd[i + 2];
+      sp4 += xd[i + 3] * yd[i + 3];
+    }
+    return sp1 + sp2 + sp3 + sp4;
+  }
+
+  @Benchmark
+  public float vectorisedSingleSumProduct() {
+    float sp = 0f;
+    for (int i = 0; i < xd.length && i < yd.length; ++i) {
+      sp += xs[i] * ys[i];
+    }
+    return sp;
+  }
+
+  @Benchmark
+  public float unrolledSingleSumProduct() {
+    float sp1 = 0f;
+    float sp2 = 0f;
+    float sp3 = 0f;
+    float sp4 = 0f;
+    float sp5 = 0f;
+    float sp6 = 0f;
+    float sp7 = 0f;
+    float sp8 = 0f;
+    for (int i = 0; i < xd.length && i < yd.length; i += 8) {
+      sp1 += xs[i] * ys[i];
+      sp2 += xs[i + 1] * ys[i + 1];
+      sp3 += xs[i + 2] * ys[i + 2];
+      sp4 += xs[i + 3] * ys[i + 3];
+      sp5 += xs[i + 4] * ys[i + 4];
+      sp6 += xs[i + 5] * ys[i + 5];
+      sp7 += xs[i + 6] * ys[i + 6];
+      sp8 += xs[i + 7] * ys[i + 7];
+    }
+    return sp1 + sp2 + sp3 + sp4 + sp5 + sp6 + sp7 + sp8;
+  }
+
+}
